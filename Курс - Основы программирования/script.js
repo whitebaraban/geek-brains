327853d4bb4992d4e327f7d5474744b308fe3a6e
Barabanov Pavel <barabanofwork@yandex.ru>
2019-01-31 08:21:01 +0300

    Сделал игру в загадки. Познакомился с операциями сравнения и логическими операциями. Узнал как работает оператор ветвления if, else if, else.

--- Курс - Основы программирования/script.js
+++ Курс - Основы программирования/script.js
@@ -1,14 +1,21 @@
 var name = prompt("Name: ");
 alert("Hello, " + name);
 
-var a = prompt(name + " введите число: ");
-var b = prompt(name + ", пожалуйста, введите второе число: ");
-a = parseInt(a);
-b = parseInt(b);
-alert("Сумма чисел " + a + " и " + b + " = " + (b + a));
+// FIRST LESSON
+//var a = prompt(name + " введите число: ");
+//var b = prompt(name + ", пожалуйста, введите второе число: ");
+//a = parseInt(a);
+//b = parseInt(b);
+//alert("Сумма чисел " + a + " и " + b + " = " + (b + a));
+
+//SECOND LESSON
 
 // Оператор ветвления
 // if (expression) {
+// command 1;
+// command 2;
+// }
+// else if () {
 // command 1;
 // command 2;
 // }
@@ -17,6 +24,41 @@ alert("Сумма чисел " + a + " и " + b + " = " + (b + a));
 // command 2;
 // }
 
-
 // Операторы сравнение
-// ..
+// a == b 
+// a != b
+// a > b
+// a < b 
+// a >= b
+// a <= b
+
+// isNan(variable) - true if not number 
+// Логические операторы
+// && - И
+// || - ИЛИ
+
+//alert('Начнем игру в загадки');
+//var correct_answear = 0;
+//
+//var first_riddle = prompt('Зимой и летом одним цветом: ');
+//if (first_riddle == "ёлка") {
+//    alert('Верно!')
+//    correct_answear += 1;
+//}
+//else {
+//    alert('Ответ неверный!')
+//}
+//
+//var second_riddle = prompt('Не лает, не кусает, а домой не пускает: ');
+//if (second_riddle == "замок") {
+//    alert('Верно!')
+//    correct_answear += 1;
+//}
+//else {
+//    alert('Ответ неверный!');
+//}
+//
+//alert("Число правильных ответов: " + correct_answear);
+
+// THIRD LESSON
+// Нарисовать алгоритм проверки попадания числа в один из двух заданных диапазонов
