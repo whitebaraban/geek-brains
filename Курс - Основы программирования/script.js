cfcc3400c038cfd6fba71573e20bbb635be32aa2
Barabanov Pavel <barabanofwork@yandex.ru>
2019-01-31 08:00:50 +0300

    Начал изучение JS

---
+++ Курс - Основы программирования/script.js
@@ -0,0 +1,22 @@
+var name = prompt("Name: ");
+alert("Hello, " + name);
+
+var a = prompt(name + " введите число: ");
+var b = prompt(name + ", пожалуйста, введите второе число: ");
+a = parseInt(a);
+b = parseInt(b);
+alert("Сумма чисел " + a + " и " + b + " = " + (b + a));
+
+// Оператор ветвления
+// if (expression) {
+// command 1;
+// command 2;
+// }
+// else {
+// command 1;
+// command 2;
+// }
+
+
+// Операторы сравнение
+// ..
